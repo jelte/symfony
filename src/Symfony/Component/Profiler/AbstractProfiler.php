02dacfd08388dd3ff9c8bde8cc326cdc061e1773
jelte <jelte@marlon.be>
2015-06-01 08:59:50 +0200

    Remove dependancy on traits for PHP 5.3

--- src/Symfony/Component/Profiler/AbstractProfiler.php
+++ src/Symfony/Component/Profiler/AbstractProfiler.php
@@ -11,7 +11,7 @@
 
 namespace Symfony\Component\Profiler;
 
-use Symfony\Component\Profiler\DataCollector\DataCollectorTrait;
+use Symfony\Component\Profiler\DataCollector\DataCollectorInterface;
 use Symfony\Component\Profiler\DataCollector\RuntimeDataCollectorInterface;
 use Symfony\Component\Profiler\DataCollector\LateDataCollectorInterface;
 use Psr\Log\LoggerInterface;
@@ -25,7 +25,10 @@ use Symfony\Component\Profiler\Storage\ProfilerStorageInterface;
  */
 abstract class AbstractProfiler
 {
-    use DataCollectorTrait;
+    /**
+     * @var DataCollectorInterface[]
+     */
+    private $collectors = [];
 
     /**
      * @var ProfilerStorageInterface
@@ -209,5 +212,68 @@ abstract class AbstractProfiler
         }
 
         return $value->getTimestamp();
+    }
+
+    /**
+     * Gets the Collectors associated with this profiler.
+     *
+     * @return array An array of collectors
+     */
+    public function all()
+    {
+        return $this->collectors;
+    }
+
+    /**
+     * Sets the Collectors associated with this profiler.
+     *
+     * @param DataCollectorInterface[] $collectors An array of collectors
+     */
+    public function set(array $collectors = array())
+    {
+        $this->collectors = array();
+        foreach ($collectors as $collector) {
+            $this->add($collector);
+        }
+    }
+
+    /**
+     * Adds a Collector.
+     *
+     * @param DataCollectorInterface $collector A DataCollectorInterface instance
+     */
+    public function add(DataCollectorInterface $collector)
+    {
+        $this->collectors[$collector->getName()] = $collector;
+    }
+
+    /**
+     * Returns true if a Collector for the given name exists.
+     *
+     * @param string $name A collector name
+     *
+     * @return bool
+     */
+    public function has($name)
+    {
+        return isset($this->collectors[$name]);
+    }
+
+    /**
+     * Gets a Collector by name.
+     *
+     * @param string $name A collector name
+     *
+     * @return DataCollectorInterface A DataCollectorInterface instance
+     *
+     * @throws \InvalidArgumentException if the collector does not exist
+     */
+    public function get($name)
+    {
+        if (!isset($this->collectors[$name])) {
+            throw new \InvalidArgumentException(sprintf('Collector "%s" does not exist.', $name));
+        }
+
+        return $this->collectors[$name];
     }
 }
